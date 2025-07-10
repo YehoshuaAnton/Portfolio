b400f46169fe3b350ae15e0129fbe0a9bb3e4c4b
Yehoshua Anton <yehoshuaanton@gmail.com>
2025-07-09 23:19:33 -0400

    Added ship placement algoritm and created a Ship class

--- Battleship/Battleship/Battleship.cs
+++ Battleship/Battleship/Battleship.cs
@@ -39,7 +39,7 @@ namespace Battleship {
             DrawLine();
             // For each row, write the corresponding uppercase letter at the start
             for (int rows = 0; rows < playerBoard.GetLength(0); rows++) {
-                Console.Write($" {(char)('A' + rows)} |");
+                Console.Write($" {(char)('A' + (rows % 26))} |");
                 // For each column in the row, display what is in the space
                 for (int columns = 0;  columns < playerBoard.GetLength(1); columns++) {
                     Console.Write($" {playerBoard[rows,columns]} |");
@@ -63,6 +63,70 @@ namespace Battleship {
                     playerBoard[row, column] = " ";
                 }
             }
+            PlaceShips();
         }
+
+        // Algorithm for randomly placing ships at the beginning of the game
+        static void PlaceShips() {
+            int horizontal, vertical;
+            Random shipOriention = new();
+            Random shipPlacement = new();
+
+            // Initialize array of Ship objects
+            Ship[] ships = [
+                new("Aircraft Carrier", 5),
+                new("Battleship", 4),
+                new("Destroyer", 3),
+                new("Submarine", 3),
+                new("Patrol Boat", 2)
+            ];
+        
+            foreach (Ship ship in ships) {
+                bool shipPlaced = false;
+                int orientation = shipOriention.Next(2);
+
+                // Pick a random starting position for the ship, making sure it won't go over the side of the board
+                while (shipPlaced == false) {
+                    if (orientation  == 0) {
+                        horizontal = shipPlacement.Next((BOARDSIZE + 1) - ship.Length);
+                        vertical = shipPlacement.Next(BOARDSIZE);
+                    } else {
+                        horizontal = shipPlacement.Next(BOARDSIZE);
+                        vertical = shipPlacement.Next((BOARDSIZE + 1) - ship.Length);
+                    }
+
+                    // Loop through the spaces the ship is trying to occupy and make sure they are all empty
+                    for (int shipPosition = 0; shipPosition < ship.Length; shipPosition++) {
+                        if (orientation == 0) {
+                            if (computerBoard[horizontal +  shipPosition, vertical] != null) {
+                                break;
+                            }
+                        } else {
+                            if (computerBoard[horizontal, vertical +  shipPosition] != null) {
+                                break;
+                            }
+                        }
+
+                        // If there are no more spaces to check, loop through the ship's length and place it on the computer's board
+                        if (shipPosition == ship.Length - 1) {
+                            for (int i  = 0; i < ship.Length; i++) {
+                                if (orientation == 0) {
+                                    computerBoard[horizontal + i, vertical] = Convert.ToString(ship.Name[0]);
+                                } else {
+                                    computerBoard[horizontal, vertical + i] = Convert.ToString(ship.Name[0]);
+                                }
+                            }
+                            shipPlaced = true;
+                        }
+                    }
+                } 
+            }
+        }
+    }
+
+    // Create a class for the ships
+    internal class Ship(string shipName, int shipLength) {
+        public string Name { get; set; } = shipName;
+        public int Length { get; set; } = shipLength;
     }
 }
